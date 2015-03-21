ebdb11dc57d04307dad07efd8d6fa2d428d42797
Christian Fei <crifei93@gmail.com>
2015-03-22 00:42:21 +0100

    incr timeout uat

--- tests/acceptance/index.js
+++ tests/acceptance/index.js
@@ -2,7 +2,7 @@ module.exports = {
   "renders markdown on the split window" : function(browser){
     browser
       .url("http://localhost:9000/src")
-      .waitForElementVisible('zen-editor', 2000)
+      .waitForElementVisible('zen-editor', 5000)
       .setValue('textarea', '# marked\n##down')
       .pause(100)
       .assert.containsText('#rendered', 'marked\ndown')
