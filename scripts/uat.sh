1b2358cc24ed8848c958b8eb771852914439fdf0
Christian Fei <crifei93@gmail.com>
2015-03-22 00:26:41 +0100

    uat with firefoc

--- scripts/uat.sh
+++ scripts/uat.sh
@@ -9,13 +9,4 @@ else
   wget http://selenium-release.storage.googleapis.com/2.45/selenium-server-standalone-2.45.0.jar -O $SELENIUM_JAR
 fi
 
-java -jar $SELENIUM_JAR > /dev/null 2>&1 &
-SELENIUM_PID=$!
-
-sleep 2
-
-
 ./node_modules/nightwatch/bin/nightwatch
-
-
-kill $SELENIUM_PID
--- tests/acceptance/index.js
+++ tests/acceptance/index.js
@@ -1,11 +1,21 @@
 module.exports = {
-  "something" : function (browser) {
+  "renders markdown on the split window" : function(browser){
     browser
-      .url("http://www.google.com")
-      .waitForElementVisible('body', 1000)
-      .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('button[name=btnG]', 1000)
-      .click('button[name=btnG]')
+      .url("http://localhost:9000/src")
+      .waitForElementVisible('zen-editor', 2000)
+      .setValue('textarea', '# marked\n##down')
+      .pause(100)
+      .assert.containsText('#rendered', 'marked\ndown')
+      .end()
+  },
+  /*
+  "persists and restores the written text": function(browser){
+    browser
+      .url("http://localhost:9000/src")
+      .waitForElementVisible('zen-editor', 2000)
+      .pause(100)
+      .assert.containsText('#rendered', 'marked\ndown')
       .end()
   }
+  */
 }
