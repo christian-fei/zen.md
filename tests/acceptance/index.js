6b3cce55a1aac17baad1b4cebfe571774fee33ed
Christian Fei <crifei93@gmail.com>
2015-03-22 15:06:12 +0100

    added uat for example text if no text present in localstorage

--- tests/acceptance/index.js
+++ tests/acceptance/index.js
@@ -1,8 +1,17 @@
 module.exports = {
+  "shows example text if no text to restore is found in the localStorage": function(browser){
+    browser
+      .url("http://localhost:9000")
+      .waitForElementVisible('zen-editor', 5000)
+      .pause(100)
+      .assert.containsText('#rendered', 'Heading 1\nHeading 2')
+      .end()
+  },
   "renders markdown on the split window" : function(browser){
     browser
       .url("http://localhost:9000")
       .waitForElementVisible('zen-editor', 5000)
+      .clearValue('textarea')
       .setValue('textarea', '# marked\n##down')
       .pause(100)
       .assert.containsText('#rendered', 'marked\ndown')
