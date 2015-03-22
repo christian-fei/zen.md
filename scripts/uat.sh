e8be92c8bd324baa6348b2290415a94020f29ee5
Christian Fei <crifei93@gmail.com>
2015-03-22 11:48:12 +0100

    fixed uat

--- scripts/uat.sh
+++ scripts/uat.sh
@@ -9,4 +9,15 @@ else
   wget http://selenium-release.storage.googleapis.com/2.45/selenium-server-standalone-2.45.0.jar -O $SELENIUM_JAR
 fi
 
+python -m SimpleHTTPServer 9000 &
+SERVER_PID=$!
+
+while curl --silent http://localhost:9000/src
+do
+  echo "."
+  sleep 0.1
+done
+
 ./node_modules/nightwatch/bin/nightwatch
+
+kill -9 $SERVER_PID
