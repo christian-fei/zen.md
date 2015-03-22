63d335568325500c84a2f4f3bf23e406faf60073
Christian Fei <crifei93@gmail.com>
2015-03-22 16:18:57 +0100

    refactored tests

--- tests/acceptance/index.js
+++ tests/acceptance/index.js
@@ -1,14 +1,17 @@
 module.exports = {
+  beforeEach: function(browser){
+    browser
+      .url("http://localhost:9000?test")
+      .waitForElementVisible('zen-editor', 1000)
+  },
   "shows example text if no text to restore is found in the localStorage": function(browser){
     browser
-      .url("http://localhost:9000")
       .pause(100)
       .assert.containsText('#rendered', 'Heading 1\nHeading 2')
       .end()
   },
   "renders markdown on the split window" : function(browser){
     browser
-      .url("http://localhost:9000")
       .clearValue('textarea')
       .setValue('textarea', '# marked\n##down')
       .pause(100)
@@ -17,13 +20,13 @@ module.exports = {
   },
   "persists and restores the written text": function(browser){
     browser
-      .url("http://localhost:9000")
       .clearValue('textarea')
       .setValue('textarea', '# marked\n##down')
       .pause(100)
       .assert.containsText('#rendered', 'marked\ndown')
 
-      .url("http://localhost:9000")
+      .url("http://localhost:9000?test")
+      .waitForElementVisible('zen-editor', 1000)
       .pause(100)
       .assert.containsText('#rendered', 'marked\ndown')
       .end()
