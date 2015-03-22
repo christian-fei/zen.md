879f88df2a08e2fad243560aff35301d28438fe1
Christian Fei <crifei93@gmail.com>
2015-03-22 15:54:48 +0100

    changed order example md

--- tests/acceptance/index.js
+++ tests/acceptance/index.js
@@ -2,7 +2,6 @@ module.exports = {
   "shows example text if no text to restore is found in the localStorage": function(browser){
     browser
       .url("http://localhost:9000")
-      .waitForElementVisible('zen-editor', 5000)
       .pause(100)
       .assert.containsText('#rendered', 'Heading 1\nHeading 2')
       .end()
@@ -10,7 +9,6 @@ module.exports = {
   "renders markdown on the split window" : function(browser){
     browser
       .url("http://localhost:9000")
-      .waitForElementVisible('zen-editor', 5000)
       .clearValue('textarea')
       .setValue('textarea', '# marked\n##down')
       .pause(100)
