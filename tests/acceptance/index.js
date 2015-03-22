01bb50956aed6a94e53b1dacc42eac3389653f25
Christian Fei <crifei93@gmail.com>
2015-03-22 16:08:36 +0100

    added uat for restore markdown from localstorage

--- tests/acceptance/index.js
+++ tests/acceptance/index.js
@@ -15,14 +15,17 @@ module.exports = {
       .assert.containsText('#rendered', 'marked\ndown')
       .end()
   },
-  /*
   "persists and restores the written text": function(browser){
     browser
       .url("http://localhost:9000")
-      .waitForElementVisible('zen-editor', 2000)
+      .clearValue('textarea')
+      .setValue('textarea', '# marked\n##down')
+      .pause(100)
+      .assert.containsText('#rendered', 'marked\ndown')
+
+      .url("http://localhost:9000")
       .pause(100)
       .assert.containsText('#rendered', 'marked\ndown')
       .end()
   }
-  */
 }
